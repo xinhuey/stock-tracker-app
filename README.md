c84933966022ed73fa83f5903bff4a372c03bf0a
xinhuey <xinhuey.wong@gmail.com>
2025-06-23 18:16:52 -0400

    fixed registration issue, able to log in now

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 1. Have an empty graph displayed on the landing page
 2. Display trends for each stock when click on symbol
+
+### Bugs
+
+1. Error loading stocks for individual user
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,6 +6,26 @@ const router = express.Router();
 
 router.post('/register', async(req, res) => {
     try{
+        const {email, password } = req.body;
+        if(!email || !password){
+            return res.status(400).json({error:'Email and password required'});
+
+        }
+        const user = new User({email, password});
+        await user.save();
+        res.status(201).json({message: 'User registered'});
+    }
+    catch(err){
+        if(err.code === 11000){
+            return res.status(400).json({error: 'Email already in use'});
+        }
+        console.error('Register Error', err);
+        res.status(500).json({error: 'Internal server error'});
+    }
+})
+
+router.post('/login', async(req, res) => {
+    try{
         const { email, password } = req.body;
         if(!email || !password){
             return res.status(400).json({error : 'Email and password are required'});
